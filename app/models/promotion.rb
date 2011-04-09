9864b1966e7f8550786918aa388a45060ddf02b6
Mark L. Chang <mark.chang@gmail.com>
2011-04-09 01:44:03 -0400

    friendly id

--- app/models/promotion.rb
+++ app/models/promotion.rb
@@ -1,6 +1,7 @@
 class Promotion < ActiveRecord::Base
   attr_accessible :position, :employer, :name, :status
   validates_presence_of :status, :position, :employer
+  has_friendly_id :name, :use_slug => true
   
   scope :recent, :order => "promotions.updated_at DESC"
 end
